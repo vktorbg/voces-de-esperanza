a3d444298fec52e3238e693fd6fd9e3fa9130fa3
vktorbg <prof.victorbriceno@gmail.com>
2025-05-24 15:29:43 -0500

    Inicio de proyecto devocionales

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,34 @@
+require("dotenv").config()
+
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Voces de Esperanza`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-source-google-spreadsheet`,
+      options: {
+        spreadsheetId: process.env.GOOGLE_SHEET_ID,
+        worksheetTitle: `Hoja1`, // o el nombre de tu pestaña
+        credentials: require("./google-credentials.json"),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Devocionales`,
+        short_name: `Devocionales`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#4b5563`, // gris neutro
+        display: `standalone`,
+        icon: `src/images/icon.jpg`, // crea este ícono
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 }
